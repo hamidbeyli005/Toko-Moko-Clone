67b1e79c616bbecda609bf04061a30e786c772fb
Hamid Hamidbeyli <hemhd.hd@gmail.com>
2022-09-07 10:07:09 +0300

    Update index.jsx

--- Frontend/src/page/home/index.jsx
+++ Frontend/src/page/home/index.jsx
@@ -1,10 +1,12 @@
 import Header from "components/Header";
+import { useSelector } from "react-redux";
 import NewPrice from "./components/NewPrice";
 import Popular from "./components/Popular";
 import Ratings from "./components/Ratings";
 import Services from "./components/Services";
 
 function Home() {
+
   return (
     <>
       <Header />
