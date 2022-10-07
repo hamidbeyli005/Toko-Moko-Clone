9c4287a02cc306eeb304cc874753d2d1c8cd06d3
Hamid Hamidbeyli <hemhd.hd@gmail.com>
2022-10-07 13:59:51 +0300

    Update categoryCtrl.js

--- Backend/controllers/categoryCtrl.js
+++ Backend/controllers/categoryCtrl.js
@@ -14,6 +14,7 @@ const categoryCtrl = {
       try {
           // if user have role = 1 ---> admin
           // only admin can create , delete and update category
+          
           const {name} = req.body;
           const category = await Category.findOne({name})
           if(category) return res.status(400).json({msg: "This category already exists."})
